101452b21ea4cb81adbce060b4464f0443e83c30
Kevin Zhang <kevinzhang19870314@gmail.com>
2023-12-20 15:50:08 +0800

    Update handle-request.ts with CORS

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -14,9 +14,10 @@ const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
 };
 
 const CORS_HEADERS: Record<string, string> = {
+  "access-control-allow-credentials": true,
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "Content-Type",
+  "access-control-allow-headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
 };
 
 export default async function handleRequest(request: NextRequest & { nextUrl?: URL }) {
