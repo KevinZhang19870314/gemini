869ed215d4684c28d5cbb473a9d901a2e9337ff3
Kevin Zhang <kevinzhang19870314@gmail.com>
2023-12-20 16:32:57 +0800

    Update handle-request.ts NextResponse

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
   const picked = new Headers();
